4b7cbd25062cc44927f193e9fe5002280ab570ef
Tshine <gtshine@gmail.com>
2022-05-21 01:33:00 +0800

    去掉不必要

--- lib/dynamic_component.dart
+++ lib/dynamic_component.dart
@@ -2,9 +2,3 @@ library dynamic_component;
 
 export 'src/dynamic_component.dart';
 export 'src/if_or_not.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
